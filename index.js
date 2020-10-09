2fcb4886870e0192d7c0ac2c0bd9f697e1eb6c4a
Robin <mehmet.ucar@imediapro.co.uk>
2020-10-09 17:30:58 +0100

    getting all buttonst

---
+++ index.js
@@ -0,0 +1,24 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+console.log(numberButtons);
+
+const operationButtons = document.querySelectorAll('[data-operation]')
+console.log(operationButtons);
+
+const equalsButton = document.querySelector('[data-equals]');
+console.log(equalsButton);
+
+const deleteButton = document.querySelector('[data-delete');
+
+console.log(deleteButton);
+
+const allClearButton = document.querySelector('[data-all-clear]');
+
+console.log(allClearButton);
+
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+
+console.log(previousOperandTextElement);
+
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+console.log(currentOperandTextElement);
